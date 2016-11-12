c2b1e6202ca4a8ea547a51c9b9a252e98efcd12c
Xiaotian Wang <xiaotianwang@XiaotiandeMacBook-Air.local>
2016-11-12 02:41:49 -0600

    Got index page, registration and dummy homepage.

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+ 'use strict';
 
 import React, { Component } from 'react';
 import {
@@ -12,42 +13,26 @@ import {
   View
 } from 'react-native';
 
-export default class WhosHungry extends Component {
+var Registration = require('./Registration');
+var ReactNative = require('react-native');
+
+class WhosHungry extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Who is hungry?',
+          component: Registration,
+        }}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('WhosHungry', () => WhosHungry);

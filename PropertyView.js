2e215d2720ca872a1c3bedec09bf8ac390a89ebd
Xiaotian Wang <xiaotianwang@XiaotiandeMacBook-Air.local>
2016-11-12 16:17:16 -0600

    Fixed bugs in property view.

--- PropertyView.js
+++ PropertyView.js
@@ -44,14 +44,6 @@ var styles = StyleSheet.create({
 class PropertyView extends Component {
 
   render() {
-    var property = this.props.property;
-    var stats = property.bedroom_number + ' bed ' + property.property_type;
-    if (property.bathroom_number) {
-      stats += ', ' + property.bathroom_number + ' ' + (property.bathroom_number > 1
-        ? 'bathrooms' : 'bathroom');
-    }
-
-    var price = property.price_formatted.split(' ')[0];
 
     return (
       <View style={styles.container}>
